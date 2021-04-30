91d980737d5e9d5d42f48d1e158df2bf2694a62b
jocelyn101325 <jocelyn101325@gmail.com>
2021-04-30 11:25:44 +0800

    人物关系搜索

--- src/views/Relation.vue
+++ src/views/Relation.vue
@@ -1,28 +1,33 @@
 <template>
 <!--  人物关系图-->
-  <div>
-    <el-form :inline="true" :model="formInline" class="demo-form-inline">
-      <el-form-item>
-        <el-input
-            placeholder="请输入姓名"
-            v-model="input"
-            clearable>
-        </el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="onSubmit">查询</el-button>
-      </el-form-item>
-    </el-form>
-    <div id="graph-chart">
-      <div id="main-chart" style="width: 100%; height: 100%;"></div>
+  <div class="main">
+    <div class="searchBox" style="width: 1100px;">
+      <input placeholder="请输入姓名" v-model="message"></input>
+      <el-button @click="onsubmit()">查询</el-button>
+      <p>{{message}}</p>
     </div>
+<!--    <p>{{graphNodes}}</p>-->
+<!--      <div v-for="item in graphNodes">{{item}}</div>-->
+<!--    <alert :message="graphNodes"></alert>-->
+    <div id="graph-chart">
+      <div id="main-chart" style="width: 100%; height: 100%;">
 
+      </div>
+    </div>
   </div>
 </template>
 <style>
 #graph-chart {
   width: 1000px;
-  height: 500px;
+  height: 750px;
+}
+.searchBox {
+  margin: 0 auto;
+}
+input{
+  height: 35px;
+  padding-left: 5px;
+
 }
 </style>
 <script>
@@ -33,146 +38,228 @@ export default {
   name: "graph-echart",
   data() {
     return {
-      graph_data: {},
+      // graphNodes: [],
+      // graphLinks: []
+      graph: {}
     };
   },
-  mounted() {
-    this.fetchData();
+  updated() {
+    this.initChart();
   },
-  created() {
-    // this.getMessage();
-  },
+  // mounted() {
+  //   this.initChart();
+  // },
+  // created() {
+  // },
   methods: {
-    fetchData: function () {
-      const _this = this;
-      this.axios
-          .get("http://127.0.0.1:5000/")
-          .then((res) => {
-            _this.initChart();
-              console.log(this.graph_data);
-            })
-          .catch((err) => {
-            console.log(err);
-          });
-      },
-
-    initChart: function () {
-      let myChart = echarts.init(document.getElementById("main-chart"));
-      let option = {
-        title: {
-          text: "Graph 简单示例",
-        },
-        tooltip: {}, //提示框
+    // getData(personName) {
+    //   //http://127.0.0.1:5000/?问号后是参数
+    //   const path = 'http://127.0.0.1:5000/';
+    //   axios.get(path+personName,).then(res=>{
+    //
+    //     this.graphNodes=res.data.nodes;
+    //     this.graphLinks=res.data.links;
+    //     // console.log(this.graphNodes)
+    //     // console.log(this.graphLinks);
+    //     this.initChart();
+    //   })
+    //       .catch((error)=>{
+    //         console.error(error);
+    //       })
+    // },
+    onsubmit() {
+      const personName = this.message
+      this.initChart(personName)
+    },
+    initChart: function(personName) {
+      this.myChart = echarts.init(document.getElementById('main-chart'));
+      this.myChart.setOption({
         animationDurationUpdate: 1500,
         animationEasingUpdate: "quinticInOut",
+        legend: {
+          show: true,
+          data: [
+            {
+              name: "0"
+            },
+            {
+              name: "1"
+            },
+            {
+              name: "2"
+            }
+          ]
+        },
         series: [
           {
             type: "graph",
             layout: "force",
-            // symbolSize: 50, //倘若该属性不在link里，则其表示节点的大小；否则即为线两端标记的大小
-            symbolSize: (value, params) => {
-              switch (params.data.category) {
-                case 0:
-                  return 100;
-                  break;
-                case 1:
-                  return 50;
-                  break;
-              }
-            },
+            symbolSize: 80, //倘若该属性不在link里，则其表示节点的大小；否则即为线两端标记的大小
+            // symbolSize: (value, params) => {
+            //   switch (params.data.category) {
+            //     case 0:
+            //       return 100;
+            //       break;
+            //     case 1:
+            //       return 70;
+            //       break;
+            //     case 2:
+            //       return 70;
+            //       break;
+            //   }
+            // },
             roam: true, //鼠标缩放功能
-            label: {
-              show: true, //是否显示标签
-            },
+            // label: {
+            //   show: true, //是否显示标签
+            // },
             focusNodeAdjacency: true, //鼠标移到节点上时突出显示结点以及邻节点和边
-            edgeSymbol: ["none", "arrow"], //关系两边的展现形式，也即图中线两端的展现形式。arrow为箭头
+            categories: [
+              {
+                name: '0',
+                itemStyle: { //可配置颜色
+                  normal: {
+                  borderColor: '#fff',
+                  borderWidth: 1,
+                  shadowBlur: 10,
+                  color: "#A170DD",
+                  }
+                }
+              }, {
+                name: '1',
+                itemStyle: { //可配置颜色
+                  normal: {
+                    borderColor: '#fff',
+                    borderWidth: 1,
+                    shadowBlur: 10,
+                    color: "#41B1EF",
+                  }
+                }
+              }, {
+                name: '2',
+                itemStyle: { //可配置颜色
+                  normal: {
+                    borderColor: '#fff',
+                    borderWidth: 1,
+                    shadowBlur: 10,
+                    color: "#E8B842",
+                  }
+                }
+              },
+              {
+                name: '3',
+                itemStyle: { //可配置颜色
+                  normal: {
+                    borderColor: '#fff',
+                    borderWidth: 1,
+                    shadowBlur: 10,
+                    color: "#91c7ae",
+                  }
+                }
+              },
+            ],
+
+            edgeSymbol: ["circle", "arrow"], //关系两边的展现形式，也即图中线两端的展现形式。arrow为箭头
             edgeSymbolSize: [4, 10],
             draggable: true,
             edgeLabel: {
-              fontSize: 20, //关系（也即线）上的标签字体大小
+              show: false,
+              normal: {
+                show: true,
+                position: 'middle',
+                textStyle: {
+                  fontSize: 12
+                },
+                formatter: "{c}"
+              },
             },
-            force: {
-              repulsion: 200,
-              edgeLength: 120,
-            },
-            itemStyle: {
-              color: function (params, value) {
-                // console.log(value);
-                switch (params.data.category) {
-                  case 0:
-                    return "#0e1f37";
-                    break;
-                  case 1:
-                    return "#8d776e";
-                    break;
-                  case 2:
-                    return "#f45249";
-                    break;
-                  case 3:
-                    return "#1b4a58";
-                    break;
-                  case 4:
-                    return "#66a4ac";
-                    break;
-                }
+            label: {
+              normal: {
+                position: "inside",
+                show: true,
+                textStyle: {
+                  color: '#fff',
+                  fontSize: 12
+                },
               }
             },
-            data: [
-              {
-                name: "节点1",
-                category: 0,
+            force: {
+              repulsion: 1000,
+              edgeLength: 300,
+            },
+            data: [],
+            links: [],
+            itemStyle: {
+              normal: {
+                color: new echarts.graphic.LinearGradient(0, 0, 1, 0, [{
+                  offset: 0,
+                  color: '#157eff'
+                }, {
+                  offset: 1,
+                  color: '#35c2ff'
+                }]),
               },
-              {
-                name: "节点2",
-                category: 1,
-              },
-              {
-                name: "节点3",
-                category: 1,
-              },
-              {
-                name: "节点4",
-                category: 1,
-              },
-            ],
-            // links: [],
-            links: [
-              {
-                source: "节点1",
-                target: "节点3",
-              },
-              {
-                source: "节点1",
-                target: "节点2",
-              },
-              {
-                source: "节点1",
-                target: "节点4",
-              },
-            ],
+              shadowBlur: 10
+            },
             lineStyle: {
               opacity: 0.9,
               width: 2,
-              curveness: 0,
+              curveness: 0.1,
             },
           },
-        ],
-      };
-      myChart.resize();  //自适应大小
-      myChart.setOption(option);
+        ]
+      });
+      const path = 'http://127.0.0.1:5000/';
+      this.axios.get(path + personName).then(res=>{
+      // this.axios.get(path).then(res=>{
+
+        this.graph=res.data;
+        // this.graphNodes=res.data.nodes;
+        // this.graphLinks=res.data.links;
+        console.log(this.graph)
+        // console.log(this.graphLinks);
+        this.myChart.setOption({
+          series:[{
+            data: this.graph.nodes,
+            links: this.graph.links
+          }],
+        })
+        });
+
+
+
     },
-    // getMessage() {
+
+
+
+
+
+
+
+
+
+
+
+
+    // getData(personName) {
+    //   //http://127.0.0.1:5000/?问号后是参数
     //   const path = 'http://127.0.0.1:5000/';
-    //   axios
-    //       .get(path)
-    //       .then(res=>{
-    //         this.msg = res.data;
-    //       })
-    //       .catch(error => {
-    //         console.error(error);
-    //       })
+    //   axios.get(path+personName,).then(res=>{
     //
-    // }
+    //     this.graphNodes=res.data.nodes;
+    //     this.graphLinks=res.data.links;
+    //     // console.log(this.graphNodes)
+    //     // console.log(this.graphLinks);
+    //     this.initChart();
+    //   })
+    //   .catch((error)=>{
+    //     console.error(error);
+    //   })
+    // },
+    // onsubmit() {
+    //   const personName = this.message
+    //   this.getData(personName)
+    // },
+
   },
 
 
--- src/views/test.vue
+++ src/views/test.vue
@@ -1,47 +1,151 @@
 <template>
+  <!--  人物关系图-->
   <div>
-    <el-form :inline="true" :model="formInline" class="demo-form-inline">
-      <el-form-item>
-        <el-input
-            placeholder="请输入内容"
-            v-model="input"
-            clearable>
-        </el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="onSubmit">查询</el-button>
-      </el-form-item>
-    </el-form>
-    <div style="width: 500px;height: 500px;border: 1px solid black">
-      <h1>{{msg}}</h1>
-    </div>
+    <input placeholder="请输入姓名" v-model="message"></input>
+    <el-button @click="onsubmit()">查询</el-button>
+    <p>{{message}}</p>
+    <!--    <p>{{graphNodes}}</p>-->
+    <!--      <div v-for="item in graphNodes">{{item}}</div>-->
+    <alert :message="graphNodes"></alert>
+    <div id="graph-chart">
+      <div id="main-chart" style="width: 100%; height: 100%;">
 
+      </div>
+    </div>
   </div>
 </template>
-
+<style>
+#graph-chart {
+  width: 1000px;
+  height: 500px;
+}
+</style>
 <script>
 import axios from 'axios';
+let echarts = require("echarts/lib/echarts");
+require("echarts/lib/chart/graph");
 export default {
+  name: "graph-echart",
   data() {
     return {
-      msg: ''
+      graphNodes: [],
+      graphLinks: []
     };
   },
+  updated() {
+    this.initChart();
+  },
+  // mounted() {
+  //   this.initChart();
+  // },
+  // created() {
+  // },
   methods: {
-    getMessage() {
+    getData(personName) {
+      //http://127.0.0.1:5000/?问号后是参数
       const path = 'http://127.0.0.1:5000/';
-      axios
-          .get(path)
-          .then(res=>{
-            this.msg = res.data;
-          })
-          .catch(error => {
+      axios.get(path+personName,).then(res=>{
+
+        this.graphNodes=res.data.nodes;
+        this.graphLinks=res.data.links;
+        // console.log(this.graphNodes)
+        // console.log(this.graphLinks);
+        this.initChart();
+      })
+          .catch((error)=>{
             console.error(error);
           })
-    }
+    },
+    onsubmit() {
+      const personName = this.message
+      this.getData(personName)
+    },
+    initChart: function() {
+      let myChart = this.echarts.init(document.getElementById('main-chart'));
+      let option = {
+        title: {
+          text: "Graph 简单示例",
+        },
+        tooltip: {}, //提示框
+        animationDurationUpdate: 1500,
+        animationEasingUpdate: "quinticInOut",
+        series: [
+          {
+            type: "graph",
+            layout: "force",
+            // symbolSize: 50, //倘若该属性不在link里，则其表示节点的大小；否则即为线两端标记的大小
+            symbolSize: (value, params) => {
+              switch (params.data.category) {
+                case 0:
+                  return 100;
+                  break;
+                case 1:
+                  return 50;
+                  break;
+              }
+            },
+            roam: true, //鼠标缩放功能
+            label: {
+              show: true, //是否显示标签
+            },
+            focusNodeAdjacency: true, //鼠标移到节点上时突出显示结点以及邻节点和边
+            edgeSymbol: ["none", "arrow"], //关系两边的展现形式，也即图中线两端的展现形式。arrow为箭头
+            edgeSymbolSize: [4, 10],
+            draggable: true,
+            edgeLabel: {
+              fontSize: 20, //关系（也即线）上的标签字体大小
+            },
+            force: {
+              repulsion: 200,
+              edgeLength: 120,
+            },
+            data: [
+              {
+                name: "节点1",
+                category: 0,
+              },
+              {
+                name: "节点2",
+                category: 1,
+              },
+              {
+                name: "节点3",
+                category: 1,
+              },
+              {
+                name: "节点4",
+                category: 1,
+              },
+            ],
+            // links: [],
+            links: [
+              {
+                source: "节点1",
+                target: "节点3",
+              },
+              {
+                source: "节点1",
+                target: "节点2",
+              },
+              {
+                source: "节点1",
+                target: "节点4",
+              },
+            ],
+            lineStyle: {
+              opacity: 0.9,
+              width: 2,
+              curveness: 0,
+            },
+          },
+        ],
+      };
+      myChart.resize();
+      myChart.setOption(option);
+    },
   },
-  created() {
-    this.getMessage();
-  }
+
+
+
 }
 </script>
